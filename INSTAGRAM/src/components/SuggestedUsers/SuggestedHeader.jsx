0aefcc831485ffe755b208e38ecaa4c0d36d45c4
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-01-06 17:54:55 +0530

    Instagram Clone

---
+++ INSTAGRAM/src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -0,0 +1,33 @@
+import { Avatar, Box, Flex, Link, Text } from "@chakra-ui/react"
+import { Link as RouterLink} from "react-router-dom"
+
+const SuggestedHeader = ()=>{
+    return <>
+        <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+            <Flex alignItems={"center"} gap={2}>
+    <Avatar name="As a programmer" size={"lg"} src="/profilepic.png" />
+    <Text fontSize={12} fontWeight={"bold"}>
+        asaprogrammer
+    </Text>
+    </Flex>
+    <Link
+    as = {RouterLink}
+    to={"/auth"}
+    fontSize={14}
+    fontWeight={"medium"}
+    color={"blue.400"}  
+    cursor={"pointer"}
+    style={{textDecoration: "none"}}
+    _hover={{
+        color: "white"
+    }}
+    transition={"0.2s ease-out"}
+    >
+        Log out
+    </Link>
+    </Flex>
+    </>
+
+}
+
+export default SuggestedHeader
